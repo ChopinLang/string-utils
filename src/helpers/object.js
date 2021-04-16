8dbf893e4612db7f4944b5f1f2a0e7153693dbcd
Jason Barr <jason@jasonsbarr.com>
2021-04-16 18:26:31 -0500

    Add function to test if null or undefined

---
+++ src/helpers/object.js
@@ -0,0 +1,13 @@
+/**
+ * Checks if `value` is null or undefined. From {@link https://github.com/panzerdp/voca/blob/master/src/helper/object/is_nil.js}
+ *
+ * @function isNil
+ * @ignore
+ * @param {*} value
+ * @returns {boolean} `true` if value is `null` or `undefined`, else `false`
+ */
+function isNil(value) {
+  return value == null;
+}
+
+export default { isNil };
