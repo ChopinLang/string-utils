657e791756ec211e8e156241778a5cd890340714
Jason Barr <jason@jasonsbarr.com>
2021-04-16 01:42:35 -0500

    Document errors

--- src/case/camelcase.js
+++ src/case/camelcase.js
@@ -5,6 +5,8 @@
  * @param {string} subject The string to camelCase
  * @param {(string|string[])} [locale=en-US] The locale or locales to use for capitalization
  * @returns {string}
+ * @throws {RangeError} if locale is not a valid language tag
+ * @throws {TypeError} if an array element is not a string
  */
 function camelCase(subject, locale = "en-US") {}
 
