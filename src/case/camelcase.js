caa2c2904a6c80af81570893f9f1f3b83007b6ec
Jason Barr <jason@jasonsbarr.com>
2021-04-16 01:37:57 -0500

    Define function header and doc for camelCase function

---
+++ src/case/camelcase.js
@@ -0,0 +1,11 @@
+/**
+ * Converts `subject` to {@link https://en.wikipedia.org/wiki/Camel_case camelCase}.
+ * @since 0.0.1
+ * @memberof Case
+ * @param {string} subject The string to camelCase
+ * @param {(string|string[])} [locale=en-US] The locale or locales to use for capitalization
+ * @returns {string}
+ */
+function camelCase(subject, locale = "en-US") {}
+
+module.exports = camelCase;
