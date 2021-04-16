a4c28a2962e5bdb29a05dfd3a9b5c68cb626f709
Jason Barr <jason@jasonsbarr.com>
2021-04-16 02:20:58 -0500

    Add additional tests

--- tests/case/camelcase.test.js
+++ tests/case/camelcase.test.js
@@ -5,4 +5,16 @@ describe("It should properly camelCase words based on their locale information",
     let subject = "These are some words";
     expect(su.camelCase(subject)).toEqual("theseAreSomeWords");
   });
+
+  test("A string that includes non-word characters", () => {
+    let subject = "++this-__is-spartaaaaa++";
+    expect(su.camelCase(subject)).toEqual("thisIsSpartaaaaa");
+  });
+
+  test("A string with non-Latin characters", () => {
+    let subject = "   _-It is istanbul   not constantinople-_ ";
+    expect(su.camelCase(subject, "TR")).toEqual(
+      "itİsİstanbulNotConstantinople"
+    );
+  });
 });
