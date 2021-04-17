d1bef7e6488c1ea2fd28c60a66023c889c1d50b5
Jason Barr <jason@jasonsbarr.com>
2021-04-17 16:56:12 -0500

    Decode UTF-8 byte array into string

---
+++ tests/encode/decodeutf8.test.js
@@ -0,0 +1,17 @@
+import su from "../../src";
+
+describe("Decode a string from a UTF-8 byte array", () => {
+  test("It should decode an encoded ASCII string", () => {
+    const input = [0x68, 0x65, 0x6c, 0x6c, 0x6f];
+    const output = "hello";
+
+    expect(su.decodeUTF8(input)).toEqual(output);
+  });
+
+  test("It should decode an encoded multibyte string", () => {
+    const input = [0xf0, 0x9f, 0x98, 0x80, 0xf0, 0x9f, 0x98, 0x81];
+    const output = "😀😁";
+
+    expect(su.decodeUTF8(input)).toEqual(output);
+  });
+});
