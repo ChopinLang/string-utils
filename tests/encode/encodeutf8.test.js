1c0687d2ed854cb2d9a6ed96fa0a084904bef1e8
Jason Barr <jason@jasonsbarr.com>
2021-04-17 16:28:53 -0500

    Encode string as UTF-8 bytes

---
+++ tests/encode/encodeutf8.test.js
@@ -0,0 +1,17 @@
+import su from "../../src/";
+
+describe("Encode a string into an array of UTF-8 code units (numeric byte values)", () => {
+  test("It should encode a string with simple ASCII characters", () => {
+    const input = "hello";
+    const output = [0x68, 0x65, 0x6c, 0x6c, 0x6f];
+
+    expect(su.encodeUTF8(input)).toEqual(output);
+  });
+
+  test("It should encode a multibyte string", () => {
+    const input = "😀😁";
+    const output = [0xf0, 0x9f, 0x98, 0x80, 0xf0, 0x9f, 0x98, 0x81];
+
+    expect(su.encodeUTF8(input)).toEqual(output);
+  });
+});
