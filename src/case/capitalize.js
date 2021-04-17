bc04620a31ba7f7f4ad4e14f9e3ad7c0b3eb3b5d
Jason Barr <jason@jasonsbarr.com>
2021-04-17 01:03:08 -0500

    Write and document capitalize function

---
+++ src/case/capitalize.js
@@ -0,0 +1,23 @@
+import { coerceToString } from "../helpers/string.js";
+
+/**
+ * Capitalizes the first letter of a string and lowercases the rest
+ *
+ * @param {string} subject String to capitalize
+ * @param {(string|string[])} [locale=en-US] The locale or locales to use
+ * @returns {string}
+ * @throws {RangeError} if locale is not a valid language tag
+ * @throws {TypeError} if an array element is not a string
+ * @example
+ * su.capitalize("thIs Is a String to CAPITALIZE")
+ * // => "This is a string to capitalize"
+ * su.capitalize("İstanbul", "TR")
+ * // => İstanbul
+ */
+export default function capitalize(subject, locale = "en-US") {
+  subject = coerceToString(subject);
+  return (
+    subject[0].toLocaleUpperCase(locale) +
+    subject.slice(1).toLocaleLowerCase(locale)
+  );
+}
