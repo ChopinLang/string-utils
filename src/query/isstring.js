a66651042fefd0d0257a6737b8abf151396c231c
Jason Barr <jason@jasonsbarr.com>
2021-04-17 16:29:34 -0500

    Add info to documentation

--- src/query/isstring.js
+++ src/query/isstring.js
@@ -1,6 +1,8 @@
 /**
  * Checks if an object is a string. From {@link https://github.com/panzerdp/voca/blob/master/src/query/is_string.js}
  *
+ * @since 0.0.1
+ * @memberof Query
  * @function isString
  * @param {*} value The value to check
  * @returns {boolean} `true` if string
