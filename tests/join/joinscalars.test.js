7515a3a046451970d6942fb7049813c0a0ce4148
Jason Barr <jason@jasonsbarr.com>
2021-04-20 17:30:08 -0500

    Write and test joinScalars function

---
+++ tests/join/joinscalars.test.js
@@ -0,0 +1,17 @@
+import su from "../../src";
+
+describe("Join an array of Unicode scalar values, often called 'characters' and represented by single Unicode code points", () => {
+  test("Join an array of simple ASCII characters", () => {
+    const input = ["h", "e", "l", "l", "o"];
+    const output = "hello";
+
+    expect(su.joinScalars(input)).toEqual(output);
+  });
+
+  test("Join an array of scalars represented by higher code points", () => {
+    const input = ["😁", "🐶", "🍕"];
+    const output = "😁🐶🍕";
+
+    expect(su.joinScalars(input)).toEqual(output);
+  });
+});
