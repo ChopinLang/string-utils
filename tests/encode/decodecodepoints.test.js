f2417baadbe2efa176e0a9278a21595962b5ceca
Jason Barr <jason@jasonsbarr.com>
2021-04-20 15:45:25 -0500

    Add test cases for characters that require multiple code points

--- tests/encode/decodecodepoints.test.js
+++ tests/encode/decodecodepoints.test.js
@@ -7,9 +7,16 @@ describe("Decode an array of Unicode code points into its string value", () => {
     expect(su.decodeCodePoints(input)).toEqual("hello");
   });
 
-  test("It should decode a string where each character is multiple code points", () => {
+  test("It should decode an array where characters are code points that require more than one UTF16 code unit", () => {
     const input = [0x1f601, 0x1f436, 0x1f355];
     const output = "😁🐶🍕";
+
+    expect(su.decodeCodePoints(input)).toEqual(output);
+  });
+
+  test("It should decode an array where some characters require multiple code points", () => {
+    const input = [0x905, 0x928, 0x941, 0x91a, 0x94d, 0x91b, 0x947, 0x926];
+    const output = "अनुच्छेद";
 
     expect(su.decodeCodePoints(input)).toEqual(output);
   });
--- tests/encode/encodecodepoints.test.js
+++ tests/encode/encodecodepoints.test.js
@@ -8,9 +8,16 @@ describe("Split a string into an array of its Unicode code points", () => {
     expect(su.encodeCodePoints(input)).toEqual(output);
   });
 
-  test("It should split a string with multiple code points per character", () => {
+  test("It should split a string with code points outside the ASCII range that require multiple UTF16 code units", () => {
     const input = "😁🐶🍕";
     const output = [0x1f601, 0x1f436, 0x1f355];
+
+    expect(su.encodeCodePoints(input)).toEqual(output);
+  });
+
+  test("It should split a string where some characters require multiple code points", () => {
+    const input = "अनुच्छेद";
+    const output = [0x905, 0x928, 0x941, 0x91a, 0x94d, 0x91b, 0x947, 0x926];
 
     expect(su.encodeCodePoints(input)).toEqual(output);
   });
