5dfd52ae4ab86b9eca2f530a7fcd244b268f741e
Jason Barr <jason@jasonsbarr.com>
2021-04-20 17:30:31 -0500

    Fix issue that was breaking tests

--- jest.config.js
+++ jest.config.js
@@ -162,7 +162,9 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: { "^.+\\.[t|j]sx?$": "babel-jest" },
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
