5a134c93d30ee0517981f7564470b092448feb4c
Jason Barr <jason@jasonsbarr.com>
2021-04-21 13:12:36 -0500

    Make words split camelCase and PascalCase words

--- src/split/words.js
+++ src/split/words.js
@@ -1,6 +1,12 @@
 import { split } from "unicode-default-word-boundary";
 import { coerceToString } from "../helpers/string.js";
-import { IS_PUNCTUATION } from "../helpers/regexp.js";
+import {
+  REGEXP_PUNCTUATION,
+  IS_PUNCTUATION,
+  REGEXP_EXTENDED_ASCII,
+  REGEXP_LATIN_WORD,
+  REGEXP_WORD,
+} from "../helpers/regexp.js";
 
 /**
  * Splits a string into words using {@link http://www.unicode.org/reports/tr29/ Unicode UAX #29} and strips punctuation
@@ -15,7 +21,24 @@ import { IS_PUNCTUATION } from "../helpers/regexp.js";
 function words(subject) {
   subject = coerceToString(subject);
   const sansUnderscores = subject.replace(/_/g, " ");
-  return split(sansUnderscores).filter((word) => !IS_PUNCTUATION.test(word));
+  let ws = split(sansUnderscores).filter((word) => !IS_PUNCTUATION.test(word));
+  ws = ws.map((w) => {
+    const regexp = REGEXP_EXTENDED_ASCII.test(w)
+      ? REGEXP_LATIN_WORD
+      : REGEXP_WORD;
+    if (regexp.test(w)) {
+      return [...w.match(regexp)];
+    }
+    return w;
+  });
+  ws = ws.reduce((acc, w) => {
+    if (Array.isArray(w)) {
+      acc.push(...w);
+      return acc;
+    }
+    return [...acc, w];
+  }, []);
+  return ws;
 }
 
 export default words;
