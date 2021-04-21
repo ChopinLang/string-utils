6ffc98e4934257ff2fa4b22bdb886ab51183ab84
Jason Barr <jason@jasonsbarr.com>
2021-04-21 09:20:02 -0500

    Add example case for no options

--- src/case/capitalize.js
+++ src/case/capitalize.js
@@ -17,8 +17,10 @@ import words from "../split/words.js";
  * @param {(string|string[])} [options.locale=en-US] The locale or locales to use
  * @returns {string}
  * @throws {RangeError} if locale is not a valid language tag
- * @throws {TypeError} if an array element is not a string
+ * @throws {TypeError} if an array element in the locale param is not a string
  * @example
+ * su.capitalize("thIs Is a String to CAPITALIZE")
+ * // => "ThIs Is a String to CAPITALIZE"
  * su.capitalize("thIs Is a String to CAPITALIZE", { lower: true })
  * // => "This is a string to capitalize"
  * su.capitalize("thIs Is a String to CAPITALIZE", { lower: true, all: true })
