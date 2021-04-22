1099d6a5d8aead09ca6e55c85cc66b0d5cc3a685
Jason Barr <jason@jasonsbarr.com>
2021-04-21 19:12:14 -0500

    Add errors thrown by function

--- src/case/downcase.js
+++ src/case/downcase.js
@@ -9,6 +9,8 @@ import { coerceToString } from "../helpers/string.js";
  * @memberof Case
  * @param {string} subject String to downcase
  * @param {string|string[]} locale Locale information or an array of locales
+ * @throws {RangeError} if locale is not a valid language tag
+ * @throws {TypeError} if an array element in the locale param is not a string
  * @example
  * su.downcase("This IS A StRiNg")
  * // => "this is a string"
